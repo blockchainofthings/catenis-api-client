48eea366998c14b02d83da83f895e9fe0e83fe18
Cláudio Sá de Castro <claudio@hiades.com.br>
2018-12-01 16:05:50 -0200

    Remove changes to project test configuration to try to avoid deprecation warnings. Instead, changes have been (locally) made to the source code of the jasmine package (and a pull request was submitted).

--- test/helpers/FixDeprecationWarnings.js
+++
@@ -1,4 +0,0 @@
-jasmine.getEnv().configure({
-    random: false,           // This replaces 'random' property on config json file
-    oneFailurePerSpec: false // This (supposedly) replaces 'stopSpecOnExceptionFailure' property on config json file
-});
